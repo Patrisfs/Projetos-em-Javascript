ffe7c376cdd8c8ceba8899a53ef925fb6bc9fc2e
patrisfs <patrickfreitas6020@gmail.com>
2022-12-08 20:37:16 -0300

    javaScript finalizado

--- Projetos Iniciantes/bin2dec/script.js
+++ Projetos Iniciantes/bin2dec/script.js
@@ -1,7 +1,13 @@
 var btn = document.querySelector("#converter");
 var bin = document.querySelector("#inserir");
+var resul = document.querySelector("#resul");
 
-btn.addEventListener("click", function(){
+function converte(){
     var bin1 = bin.value;
-    
+    var dec = parseInt(bin1, 2);
+    return dec;
+};
+btn.addEventListener("click", function(){
+    var dec =converte(); 
+    resul.value = dec
 });
